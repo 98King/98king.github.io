2ee918d3ac994d1e989871535c625e2ae1da7efc
98King <35552581+98King@users.noreply.github.com>
2025-04-16 01:31:58 +0000

    更新文章 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/15/hello-world/","2023/07/19/三伏天突袭南京/"];function toRandomPost(){
+var posts=["2025/04/16/hello-world/","2023/07/19/三伏天突袭南京/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
